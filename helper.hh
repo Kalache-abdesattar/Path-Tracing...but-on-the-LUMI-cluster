4ed07a294db7624c91ebba2341310b75e5047f3d
Kalache-abdesattar <abdesattar.kalache@tuni.fi>
2024-02-11 19:22:26 +0200

    organized chrono functions

--- helper.hh
+++ helper.hh
@@ -1,4 +1,15 @@
 #ifndef HELPER_HH
 #define HELPER_HH
 
+
+#include <iostream>
+#include <chrono>
+
+
+std::ostream& operator<<(std::ostream& out, std::chrono::nanoseconds diff)
+{
+    out << std::chrono::duration_cast<std::chrono::milliseconds>(diff).count(); 
+    return out;
+}
+
 #endif 
